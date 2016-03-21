d191ad3aea544fdd84c45bdfd22f6f1d1e48297b
Ira Shchukina <schukinaira@gmail.com>
2016-03-21 15:43:32 +0300

    add collections; submit draft

--- src/main/java/ru/spbau/mit/Collections.java
+++ src/main/java/ru/spbau/mit/Collections.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 public final class Collections {
 
-    private Collections() {}
+    private Collections() {
+    }
 
     public static <T, R> Collection<R> map(Function<T, R> f, Iterable<? extends T> collection) {
         List<R> result = new ArrayList<>();
@@ -15,4 +16,52 @@ public final class Collections {
         }
         return result;
     }
+
+    public static <T> Collection<T> filter(Predicate<T> p, Iterable<? extends T> collection) {
+        List<T> result = new ArrayList<>();
+        for (T elem : collection) {
+            if (p.apply(elem)) {
+                result.add(elem);
+            }
+        }
+        return result;
+    }
+
+    public static <T> Collection<T> takeWhile(Predicate<T> p, Iterable<? extends T> collection) {
+        List<T> result = new ArrayList<>();
+        for (T elem : collection) {
+            if (!p.apply(elem)) {
+                break;
+            }
+            result.add(elem);
+        }
+        return result;
+    }
+
+    public static <T> Collection<T> takeUnless(Predicate<T> p, Iterable<? extends T> collection) {
+        return takeWhile(p.not(), collection);
+    }
+
+    public static <T, R> R foldl(Function2<R, T, R> f, R start, Iterable<? extends T> collection) {
+        R result = start;
+        for (T elem : collection) {
+            result = f.apply(result, elem);
+        }
+        return result;
+    }
+
+    public static <T, R> R foldr(Function2<? super T, ? super R, ? extends R> f,
+                                 R start, Iterable<? extends T> collection) {
+        R result = start;
+        List<T> copyCollection = new ArrayList<>();
+        for (T elem : collection) {
+            copyCollection.add(elem);
+        }
+
+        for (int i = copyCollection.size() - 1; i >= 0; i--) {
+            result = f.apply(copyCollection.get(i), result);
+        }
+        return result;
+    }
+
 }
--- src/test/java/ru/spbau/mit/CollectionsTest.java
+++ src/test/java/ru/spbau/mit/CollectionsTest.java
@@ -7,19 +7,67 @@ import java.util.Collection;
 import java.util.List;
 
 import static ru.spbau.mit.Function1Test.CLASS_NAME;
+import static ru.spbau.mit.PredicateTest.IS_EVEN;
 import static org.junit.Assert.assertEquals;
 
 public class CollectionsTest {
 
-    private static final int THREE = 3;
+    private static final Integer TWO = 2;
+    private static final Integer THREE = 3;
+    private static final Integer SIX = 6;
 
     @Test
     public void testMap() throws Exception {
-        List<Integer> numbers = Arrays.asList(1, 2, THREE);
+        List<Integer> numbers = Arrays.asList(1, TWO, THREE);
         Collection<String> results = Collections.map(CLASS_NAME, numbers);
         assertEquals(numbers.size(), results.size());
         for (String result : results) {
             assertEquals("java.lang.Integer", result);
         }
     }
+
+    @Test
+    public void testFilter() throws Exception {
+        List<Integer> numbers = Arrays.asList(1, TWO, THREE);
+        Collection<Integer> results = Collections.filter(IS_EVEN, numbers);
+        assertEquals(1, results.size());
+        for (Integer result : results) {
+            assertEquals(TWO, result);
+        }
+    }
+
+    @Test
+    public void testTakeWhile() throws Exception {
+        List<Integer> numbers = Arrays.asList(1, TWO, THREE);
+        Collection<Integer> results = Collections.takeWhile(IS_EVEN, numbers);
+        assertEquals(0, results.size());
+    }
+
+    @Test
+    public void testTakeUnless() throws Exception {
+        List<Integer> numbers = Arrays.asList(1, TWO, THREE);
+        Collection<Integer> results = Collections.takeUnless(IS_EVEN, numbers);
+        assertEquals(1, results.size());
+    }
+
+    @Test
+    public void testFoldl() throws Exception {
+        List<Integer> numbers = Arrays.asList(1, TWO, THREE);
+        assertEquals(SIX, Collections.foldl(SUM, 0, numbers));
+
+    }
+
+    @Test
+    public void testFoldr() throws Exception {
+        List<Integer> numbers = Arrays.asList(1, TWO, THREE);
+        assertEquals(SIX, Collections.foldr(SUM, 0, numbers));
+
+    }
+
+    public static final Function2<Integer, Integer, Integer> SUM = new Function2<Integer, Integer, Integer>() {
+        @Override
+        public Integer apply(Integer t1, Integer t2) {
+            return t1 + t2;
+        }
+    };
 }
